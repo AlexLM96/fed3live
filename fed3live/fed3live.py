6685f4ea56696bbae56de2d77dba4fdee558897b
Alex Legaria <leg.alex96@gmail.com>
2023-02-10 15:49:17 -0600

    Added csv functionality

--- fed3live/fed3live.py
+++ fed3live/fed3live.py
@@ -12,13 +12,23 @@ import statsmodels.api as sm
 
 #%%
 
-def pellets_per_day(data_choices):
-    c_diff = np.diff(data_choices["fed3BlockPelletCount"])
+def pellets_per_day(data_choices, mode="database"):
+    if mode == "database":
+        block_pellet_count = "fed3BlockPelletCount"
+        date_time = "fed3Time"
+    elif mode == "csv":
+        block_pellet_count = "Block_Pellet_COunt"
+        date_time = "MM:DD:YYYY hh:mm:ss"
+        data_choices[date_time] = pd.to_datetime(data_choices[date_time])
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
+    c_diff = np.diff(data_choices[block_pellet_count])
     c_diff2 = np.where(c_diff < 0, 1, c_diff)
     c_pellets = int(c_diff2.sum())
     
-    c_start = data_choices["fed3Time"].iloc[0].date()
-    c_end = data_choices["fed3Time"].iloc[-1].date()
+    c_start = data_choices[date_time].iloc[0].date()
+    c_end = data_choices[date_time].iloc[-1].date()
     c_days = (c_end-c_start).days
     
     c_ppd = c_pellets/c_days
@@ -39,17 +49,26 @@ def binned_paction(actions, window):
         
     return p_left
 
-def reversal_peh(data_choices, min_max, return_avg = False):
-    switches = np.where(np.diff(data_choices["fed3DeviceNumber"]) != 0)[0] + 1
+def reversal_peh(data_choices, min_max, return_avg = False, mode="database"):
+    if mode == "database":
+        device_number = "fed3DeviceNumber"
+        event = "fed3EventActive"
+    elif mode == "csv":
+        device_number = "Device_Number"
+        event = "Event"
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
+    switches = np.where(np.diff(data_choices[device_number]) != 0)[0] + 1
     switches = switches[np.logical_and(switches+min_max[0] > 0, switches+min_max[1] < data_choices.shape[0])]
-    #print(switches)
+
     all_trials = []
     for switch in switches:
         c_trial = np.zeros(np.abs(min_max[0])+min_max[1])
         counter = 0
         for i in range(min_max[0],min_max[1]):
-            c_choice = data_choices["fed3EventActive"].iloc[switch+i]
-            c_prob_right = data_choices["fed3DeviceNumber"].iloc[switch+i]
+            c_choice = data_choices[event].iloc[switch+i]
+            c_prob_right = data_choices[event].iloc[switch+i]
             if c_prob_right < 50:
                 c_high = "Left"
             elif c_prob_right > 50:
@@ -73,16 +92,25 @@ def reversal_peh(data_choices, min_max, return_avg = False):
     else:
         return aall_trials
     
-def win_lose_action(data_choices):
+def win_lose_action(data_choices, mode):
+    if mode == "database":
+        block_pellet_count = "fed3BlockPelletCount"
+        event = "fed3EventActive"
+    elif mode == "csv":
+        block_pellet_count = "Block_Pellet_Count"
+        event = "Event"
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
     win_stay = 0
     win_shift = 0
     lose_stay = 0
     lose_shift = 0
     for i in range(data_choices.shape[0]-1):
-        c_choice = data_choices["fed3EventActive"].iloc[i]
-        next_choice = data_choices["fed3EventActive"].iloc[i+1]
-        c_count = data_choices["fed3BlockPelletCount"].iloc[i]
-        next_count = data_choices["fed3BlockPelletCount"].iloc[i+1]
+        c_choice = data_choices[event].iloc[i]
+        next_choice = data_choices[event].iloc[i+1]
+        c_count = data_choices[block_pellet_count].iloc[i]
+        next_count = data_choices[block_pellet_count].iloc[i+1]
         if np.logical_or(next_count-c_count == 1, next_count-c_count == -19):
             c_outcome = 1
         else:
@@ -113,21 +141,30 @@ def win_lose_action(data_choices):
     
     return win_stay_p, lose_shift_p
 
-def side_rewards(data_choices):
+def side_rewards(data_choices, mode):
+    if mode == "database":
+        block_pellet_count = "fed3BlockPelletCount"
+        event = "fed3EventActive"
+    elif mode == "csv":
+        block_pellet_count = "Block_Pellet_Count"
+        event = "Event"
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
     left_reward = []
     right_reward = []
     for i in range(data_choices.shape[0]-1):
         c_event = data_choices.iloc[i,:]
-        c_count = c_event["fed3BlockPelletCount"]
-        next_count = data_choices["fed3BlockPelletCount"].iloc[i+1]
-        if c_event["fed3EventActive"] == "Left":
+        c_count = c_event[block_pellet_count]
+        next_count = data_choices[block_pellet_count].iloc[i+1]
+        if c_event[event] == "Left":
             right_reward.append(0)
             if np.logical_or(next_count - c_count == 1, next_count - c_count == -19):
                 left_reward.append(1)
             else:
                 left_reward.append(0)
         
-        elif c_event["fed3EventActive"] == "Right":
+        elif c_event[event] == "Right":
             left_reward.append(0)
             if np.logical_or(next_count - c_count == 1, next_count - c_count == -19):
                 right_reward.append(1)
@@ -136,12 +173,19 @@ def side_rewards(data_choices):
                 
     return left_reward, right_reward
 
-def create_X(data_choices, left_reward, right_reward, n_feats):
+def create_X(data_choices, left_reward, right_reward, n_feats, mode):
+    if mode == "database":
+        event = "fed3EventActive"
+    elif mode == "csv":
+        event = "Event"
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
     reward_diff = np.subtract(left_reward,right_reward)
     X_dict = {}
     for i in range(data_choices.shape[0]-n_feats):
         c_idx = i + n_feats
-        X_dict[c_idx+1] = [data_choices["fed3EventActive"].iloc[c_idx]]
+        X_dict[c_idx+1] = [data_choices[event].iloc[c_idx]]
         for j in range(n_feats):
             X_dict[c_idx+1].append(reward_diff[c_idx-(j+1)])
             
@@ -163,15 +207,26 @@ def logit_regr(X_df):
      return c_regr
  
 
-def pokes_per_pellet(data_choices):
-    c_diff = np.diff(data_choices["fed3BlockPelletCount"])
+def pokes_per_pellet(data_choices, mode):
+    if mode == "database":
+        block_pellet_count = "fed3BlockPelletCount"
+        left_count = "fed3LeftCount"
+        right_count = "fed3RightCount"
+    elif mode == "csv":
+        block_pellet_count = "Block_Pellet_Count"
+        left_count = "Left_Poke_Count"
+        right_count = "Right_Poke_Count"
+    else:
+        raise ValueError(f"Expected 'database' or 'csv' but got {mode} instead")
+    
+    c_diff = np.diff(data_choices[block_pellet_count])
     c_diff2 = np.where(c_diff < 0, 1, c_diff)
     c_pellets = int(c_diff2.sum())
     
-    c_left_diff = np.diff(data_choices["fed3LeftCount"])
+    c_left_diff = np.diff(data_choices[left_count])
     c_left_diff2 = np.where(np.logical_or(c_left_diff < 0, c_left_diff > 1), 1, c_left_diff)
     
-    c_right_diff = np.diff(data_choices["fed3RightCount"])
+    c_right_diff = np.diff(data_choices[right_count])
     c_right_diff2 = np.where(np.logical_or(c_right_diff < 0, c_right_diff > 1), 1, c_right_diff)
     
     all_pokes = c_left_diff2.sum() + c_right_diff2.sum()
